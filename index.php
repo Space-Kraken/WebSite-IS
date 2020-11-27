2771be31576b784c53ab2a0dbae91b099cd6728c
Ernesto <ridik_ecp@hotmail.com>
2020-11-27 13:11:58 -0600

    Add files an restructure proyect dir
    Add index and views dir of the proyec.
    restructure css dir (assests contain css dir)

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include("./views/header.php") ?>
+<button type="button" class="btn btn-primary">Test</button>
+<i class="material-icons">qr_code</i>
+<?php include("./views/footer.php") ?>
---
+++ views/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- bootswach -->
+    <link href="./assets/bootstrap/bootstrap.css" rel="stylesheet" />
+    <link href="./assets/bootstrap/bootstrap.min.css" rel="stylesheet" />
+    <!-- Material UI - Icons -->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
+    <title>IS page</title>
+  </head>
+  <body>
+    <header>
+      <!-- navbar -->
+    </header>
