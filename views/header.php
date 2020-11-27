c6921c02f329063c6aea229e2075ca5065f071b7
Emmanuel Cortes <manetoso98@hotmail.com>
2020-11-27 13:24:35 -0600

    Navbar addition
    The component navbar have been created

--- views/header.php
+++ views/header.php
@@ -11,9 +11,31 @@
       rel="stylesheet"
       href="https://fonts.googleapis.com/icon?family=Material+Icons"
     />
+    <link rel="stylesheet" href="./assets/styles/navbar.css">
     <title>IS page</title>
   </head>
   <body>
     <header>
-      <!-- navbar -->
+    <div id="nav">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">GUI-NTELLIGENCE</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="material-icons">menu</span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarColor01">
+                    <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
     </header>
